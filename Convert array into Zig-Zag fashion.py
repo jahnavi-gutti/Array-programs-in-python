8b1109cbf1c44229abcb3af9baf3ecd49966c12d
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-08-02 13:51:35 +0530

    Update Convert array into Zig-Zag fashion.py

--- Convert array into Zig-Zag fashion.py
+++ Convert array into Zig-Zag fashion.py
@@ -1,3 +1,4 @@
+#Given an array Arr (distinct elements) of size N. Rearrange the elements of array in zig-zag fashion. The converted array should be in form a < b > c < d > e < f. The relative order of elements is same in the output i.e you have to iterate on the original array only
 n=int(input())
 a= [int(x) for x in input().strip(" ").split(" ")]
 f=True
