d4b708d48b16121a51538d18a0d668acd235b865
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-08-02 13:47:17 +0530

    Update Minimum platforms.py

--- Minimum platforms.py
+++ Minimum platforms.py
@@ -1,3 +1,6 @@
+#Given arrival and departure times of all trains that reach a railway station. Find the minimum number of platforms required for the railway station so that no train is kept waiting.
+#Consider that all the trains arrive on the same day and leave on the same day. Arrival and departure time can never be the same for a train but we can have arrival time of one train equal to departure time of the other. At any given instance of time, same platform can not be used for both departure of a train and arrival of another train. In such cases, we need different platforms.
+
 x=int(input())
 arrival = [int(i) for i in input().strip().split(" ")]
 departure =[ int(i) for i in input().strip().split(" ")]
