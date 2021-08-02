b8dd47c801dfe7d8cf91f77fa85a367d010749a5
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-08-02 13:50:54 +0530

    Update Element with left side smaller and right side greater .py

--- Element with left side smaller and right side greater .py
+++ Element with left side smaller and right side greater .py
@@ -1,3 +1,6 @@
+#Given an unsorted array of size N. Find the first element in array such that all of its left elements are smaller and all right elements to it are greater than it.
+
+Note: Left and right side elements can be equal to required element. And extreme elements cannot be required element.
 t = int(input())
 for i in range(t):
     n = int(input())
