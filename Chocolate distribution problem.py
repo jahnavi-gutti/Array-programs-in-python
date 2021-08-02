462f2fc6eb78c92c628c745a3baa1d13c22fb9a4
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-08-02 13:50:09 +0530

    Update Chocolate distribution problem.py

--- Chocolate distribution problem.py
+++ Chocolate distribution problem.py
@@ -1,3 +1,6 @@
+#Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+#1. Each student gets exactly one packet.
+#2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.
 n=int(input())
 m=int(input())
 a=list(map(int, input().strip().split(" ")))
