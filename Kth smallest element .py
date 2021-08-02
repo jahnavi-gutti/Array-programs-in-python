8a93d31d34dfaa2a62bfe06ac754f05f257e8b9c
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-08-02 13:48:24 +0530

    Update Kth smallest element .py

--- Kth smallest element .py
+++ Kth smallest element .py
@@ -1,3 +1,4 @@
+#Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
 n=int(input())
 k=int(input())
 a = [int(i) for i in input().strip().split(" ")]
