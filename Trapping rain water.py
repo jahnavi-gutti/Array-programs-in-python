ab596f4ca9d4132d6ecaa5b128f74ed89ba7f93f
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-08-02 13:36:29 +0530

    Update Trapping rain water.py

--- Trapping rain water.py
+++ Trapping rain water.py
@@ -1,3 +1,5 @@
+#Given an array arr[] of N non-negative integers representing the height of blocks. If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
+
 n=int(input())
 a = [int(val) for val in input().strip().split(" ")]
 w=0
