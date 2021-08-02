a75060ca8c0a2c8c59ad930bdffb3e2200bcc728
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-08-02 13:41:42 +0530

    Update Merge two sorted array.py

--- Merge two sorted array.py
+++ Merge two sorted array.py
@@ -1,3 +1,5 @@
+#Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order without using any extra space. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+ 
 n=int(input())
 a=[int(x) for x in input().strip().split(" ")]
 m=int(input())
